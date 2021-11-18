86928c85839d03f007bcf079d498ee00dfd88e23
Jayashree Sridhar <jayashree.sridhar@sjsu.edu>
2021-11-17 23:30:54 -0800

    Adding exec to server

--- assignment2/server.c
+++ assignment2/server.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <pwd.h>
 #define PORT 8080
 
 int main(int argc, char const *argv[])
@@ -16,47 +17,93 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0};
     char *hello = "Hello from server";
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+    printf("argv=%p\n", *argv);
+
+    if (strcmp(argv[0], "EXEC_PROCESS") == 0) {
+        printf("Child exec process \n");
+        // privilege dropping on new child process
+        new_socket = atoi(argv[1]);
+
+        // get nobody info
+        struct passwd *nobodyUser = getpwnam("nobody");
+        printf("nobody id: %d \n", nobodyUser->pw_uid);
+        if (setuid(nobodyUser->pw_uid) == -1) {
+            perror("Privilege dropping failed");
+            exit(EXIT_FAILURE);
+        } else {
+            printf("Privilege dropping successful \n");
+
+            valread = read(new_socket, buffer, 1024);
+            printf("Read %d bytes: %s\n", valread, buffer);
+            send(new_socket, hello, strlen(hello), 0);
+            printf("Hello message sent\n");
+        }
+    } else {
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+        &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( PORT );
+
+        if (bind(server_fd, (struct sockaddr *)&address,
+        sizeof(address)) < 0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                        (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        pid_t pid = fork();
+        int status = 0;
+
+        if (pid < 0) {
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) { // process client code only in child process
+            
+            // Create a new process of child
+            char address[10];
+            snprintf(address,10,"%d",new_socket);            
+            char *argv1[] = {"EXEC_PROCESS", address, NULL};
+
+            int status = execvp("./server", argv1);
+            if(status < 0){
+                perror("Child process exec failed\n");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // parent waits for child to finish and then exit
+            waitpid(pid, &status, 0);
+            exit(0);        
+        }
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-	&opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    
 
-    if (bind(server_fd, (struct sockaddr *)&address,
-	sizeof(address)) < 0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
-    }
-
-    valread = read(new_socket, buffer, 1024);
-    printf("Read %d bytes: %s\n", valread, buffer);
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
 
     return 0;
 }
